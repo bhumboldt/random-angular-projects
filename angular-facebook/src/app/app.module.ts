ac6c1eb4a5553195bf66e1876d138cd3a4aac994
bhumboldt <brandon@cockpitmobile.com>
2022-06-04 11:35:53 -0500

    Setup initial structure, use css-flex

--- angular-facebook/src/app/app.module.ts
+++ angular-facebook/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopNavComponent } from './core/top-nav/top-nav.component';
+import { BaseComponent } from './core/base/base.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavComponent,
+    BaseComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular-facebook/src/app/home/home.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { ContactsComponent } from './contacts/contacts.component';
+import { FeedItemComponent } from './feed-item/feed-item.component';
+import { StoryListComponent } from './story-list/story-list.component';
+import { StoryComponent } from './story/story.component';
+import { UserFeedComponent } from './user-feed/user-feed.component';
+
+
+@NgModule({
+  declarations: [
+    UserFeedComponent,
+    FeedItemComponent,
+    StoryListComponent,
+    StoryComponent,
+    ContactsComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule
+  ]
+})
+export class HomeModule { }
